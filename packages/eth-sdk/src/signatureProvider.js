bf7b9c64eebd997e6fb9c4bd1ae8b672d1499e86
Phil <qft.gtr@gmail.com>
2021-02-21 14:57:21 +0800

    Update signatureProvider.js

--- packages/eth-sdk/src/signatureProvider.js
+++ packages/eth-sdk/src/signatureProvider.js
@@ -3,7 +3,7 @@ import { ethers } from 'ethers'
 
 export default function signatureProvider (address) {
   return async tx => {
-    const privateKey = await keypairManager.getSecret(address)
+    const privateKey = await keypairManager.getSecret(address.toLowerCase())
     const wallet = new ethers.Wallet(privateKey)
     return await wallet.signTransaction(tx)
   }
