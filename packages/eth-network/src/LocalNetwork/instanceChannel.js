fbf60d23281f03fb8a7448b679f5fc55adbd0d49
Phil Li <qft.gtr@gmail.com>
2021-04-14 11:28:48 +0800

    no need for DOCKER_IMAGE_NODE_MIN_VERSION

--- packages/eth-network/src/LocalNetwork/instanceChannel.js
+++ packages/eth-network/src/LocalNetwork/instanceChannel.js
@@ -4,13 +4,8 @@ import semver from 'semver'
 
 const channel = new IpcChannel('node-instance')
 
-const opts = {
+channel.node = new DockerImageChannel(process.env.DOCKER_IMAGE_NODE, {
   filter: v => semver.valid(v)
-}
-if (process.env.DOCKER_IMAGE_NODE_MIN_VERSION) {
-  opts.filter = v => semver.valid(v) && semver.gte(v, process.env.DOCKER_IMAGE_NODE_MIN_VERSION)
-}
-
-channel.node = new DockerImageChannel(process.env.DOCKER_IMAGE_NODE, opts)
+})
 
 export default channel
