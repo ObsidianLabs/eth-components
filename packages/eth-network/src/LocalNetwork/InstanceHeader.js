dcc9c7f5165f1c141656a1061fd74813a90df668
Phil Li <qft.gtr@gmail.com>
2021-05-23 21:59:29 +0800

    Update InstanceHeader.js

--- packages/eth-network/src/LocalNetwork/InstanceHeader.js
+++ packages/eth-network/src/LocalNetwork/InstanceHeader.js
@@ -8,7 +8,7 @@ export default function InstanceHeader () {
         <th style={{ width: '20%' }} />
         <th style={{ width: '15%' }}>node version</th>
         <th style={{ width: '15%' }}>chain</th>
-        <th style={{ width: '10%' }}>blocks</th>
+        <th style={{ width: '10%' }}>height</th>
         <th style={{ width: '15%', textAlign: 'right' }}></th>
       </tr>
     </thead>
