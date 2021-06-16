231c0b50590f4936ce63eb3c36ade873c6fd2f14
Phil Li <qft.gtr@gmail.com>
2021-06-15 22:30:47 -0700

    update rpc

--- packages/eth-sdk/src/rpc/prepare.js
+++ packages/eth-sdk/src/rpc/prepare.js
@@ -1,14 +1,34 @@
-export default parameters => {
-  console.log(parameters)
+function prepare (parameters, asObject) {
   const obj = parameters.obj
-  return Object.keys(obj).map(key => {
+  const keys = Object.keys(obj)
+  const values = keys.map(key => {
     const param = obj[key]
-    if (key === 'blockNumber') {
-      if (param.value === '0') {
-        return 'latest'
-      }
-      return `0x${BigInt(param.value).toString(16)}`
+    if (param.type === 'tuple') {
+      return prepare({ obj: param.value }, true)
     }
-    return param.value 
+    if (param.type.endsWith('[]') && !param.value.length) {
+      return null
+    }
+    if (param.type.startsWith('uint')) {
+      let value = param.value
+      if (value === '0') {
+        if (key === 'gas') {
+          value = '500000000'
+        } else if (key === 'gasPrice') {
+          value = '1'
+        } else if (key === 'blockNumber') {
+          return 'latest'
+        }
+      }
+      return `0x${BigInt(value).toString(16)}`
+    }
+    return param.value
   })
+  if (asObject) {
+    return Object.fromEntries(keys.map((k, i) => [k, values[i]]))
+  } else {
+    return values
+  }
 }
+
+export default prepare
