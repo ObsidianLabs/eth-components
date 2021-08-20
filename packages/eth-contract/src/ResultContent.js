a83e21a9ec4bdaa0cf2c317941940704591ccf28
Phil Li <qft.gtr@gmail.com>
2021-08-20 15:03:47 +0800

    Update ResultContent.js

--- packages/eth-contract/src/ResultContent.js
+++ packages/eth-contract/src/ResultContent.js
@@ -1,14 +1,11 @@
 import React from 'react'
 
-import {
-  ButtonOptions,
-} from '@obsidians/ui-components'
-
 import notification from '@obsidians/notification'
 import keypairManager from '@obsidians/keypair'
 
 import { withRouter } from 'react-router-dom'
 import ReactJson from 'react-json-view'
+import Highlight from 'react-highlight'
 
 export default withRouter(({ format, actionError, actionResult, history }) => {
   const [keypairs, setKeypairs] = React.useState({})
@@ -45,9 +42,9 @@ export default withRouter(({ format, actionError, actionResult, history }) => {
       )
     } else {
       return (
-        <pre className='text-body pre-wrap break-all small user-select'>
-          {JSON.stringify(actionResult.raw, null, 2)}
-        </pre>
+        <Highlight language='javascript' className='pre-wrap break-all small' element='pre'>
+          <code>{JSON.stringify(actionResult.raw, null, 2)}</code>
+        </Highlight>
       )
     }
   }
