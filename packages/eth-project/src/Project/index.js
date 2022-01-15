20bfe13dad11954b22afee540b6983659a7a27e6
北屿一木 <20736452+iNorthIsle@users.noreply.github.com>
2022-01-15 11:05:17 +0800

    hotfix: drop some dev commit (#24)
    * fix: not the root config file will open project setting
    
    * fix: explorer & contract tab close event
    
    * fixup! fix: not the root config file will open project setting

--- packages/eth-project/src/Project/index.js
+++ packages/eth-project/src/Project/index.js
@@ -2,7 +2,7 @@ import Workspace from '@obsidians/workspace'
 import fileOps from '@obsidians/file-ops'
 import { useBuiltinCustomTabs, modelSessionManager, defaultModeDetector } from '@obsidians/code-editor'
 import compilerManager, { CompilerTerminal } from '@obsidians/compiler'
-
+import platform from '@obsidians/platform'
 import ProjectManager from '../ProjectManager'
 
 import ProjectToolbar from './ProjectToolbar'
@@ -14,14 +14,18 @@ import findIndex from 'lodash/findIndex'
 useBuiltinCustomTabs(['markdown'])
 modelSessionManager.registerCustomTab('settings', ProjectSettingsTab, 'Project Settings')
 modelSessionManager.registerModeDetector(filePath => {
-  const { base } = fileOps.current.path.parse(filePath)
-  if (base === 'config.json') {
-    return 'settings'
-  } else if (base.endsWith('.sol')) {
-    return 'solidity'
-  } else {
-    return defaultModeDetector(filePath)
-  }
+    const { prefix, userId, projectId, settingsFilePath } = modelSessionManager.projectManager
+    const { base } = fileOps.current.path.parse(filePath)
+    const settingFilePath = platform.isDesktop ? settingsFilePath : `${prefix}/${userId}/${projectId}/config.json`
+    const isRoot = settingFilePath === filePath
+
+    if (base === 'config.json' && isRoot) {
+      return 'settings'
+    } else if (base.endsWith('.sol')) {
+      return 'solidity'
+    } else {
+      return defaultModeDetector(filePath)
+    }
 })
 
 const makeContextMenu = (contextMenu, projectManager) => node => {
