95c65d52729229b67fe91bfe2e39867566d789ae
iNorthIsle <ndobox@outlook.com>
2022-02-24 14:53:08 +0800

    feat: change the solc resource url

--- packages/eth-compiler/src/compilerManager.js
+++ packages/eth-compiler/src/compilerManager.js
@@ -75,8 +75,9 @@ export class CompilerManager {
 
     const solcVersion = projectManager.projectSettings.get('compilers.solc')
     const solcFileName = soljsonReleases[solcVersion]
-    const solcUrl = `/solc/${solcFileName}`
-    // const solcUrl = `https://solc-bin.ethereum.org/bin/${solcFileName}`
+
+    // TODO: use the production proxy temporally
+    const solcUrl = `https://eth.ide.black/solc/${solcFileName}`
 
     const evmVersion = projectManager.projectSettings.get('compilers.evmVersion')
     const optimizer = projectManager.projectSettings.get('compilers.optimizer')
