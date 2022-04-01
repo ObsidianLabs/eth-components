34fdc5c0a6a04b44261c2c53aaab2aa1182c4f5d
flowfire <ziyi.meng@obsidians.io>
2022-04-01 23:44:33 +0800

    fix abi bugs update to dart-sass add new net work

--- packages/eth-explorer/src/buttons/FaucetButton.js
+++ packages/eth-explorer/src/buttons/FaucetButton.js
@@ -15,6 +15,10 @@ export default class FaucetButton extends PureComponent {
       faucetUrl = `https://faucet.rinkeby.io/`
     } else if (this.props.network === 'kovan') {
       faucetUrl = `https://faucet.kovan.network/`
+    } else if (this.props.network === 'evmostest') {
+      faucetUrl = `https://faucet.evmos.org/`
+    } else if (this.props.network === 'bnbtest') {
+      faucetUrl = `https://testnet.binance.org/faucet-smart`
     } else {
       return
     }
