12e90fd8d73a45fc48cf15fe950c85290295e6a4
Tab <d491405879@gmail.com>
2022-06-10 18:25:27 +0800

    feat: add matamask option (#134)

--- packages/eth-contract/src/components/ContractForm/ParamInput/AddressParamInput.js
+++ packages/eth-contract/src/components/ContractForm/ParamInput/AddressParamInput.js
@@ -1,5 +1,4 @@
 import React from 'react'
-
 import { networkManager } from '@obsidians/eth-network'
 import { KeypairInputSelector } from '@obsidians/keypair'
 
@@ -21,11 +20,12 @@ export default function AddressParamInput ({ size, value, onChange, disabled, ma
       extra={networkManager.browserExtension?.isEnabled && [{
         group: networkManager.browserExtension.name.toLowerCase(),
         badge: networkManager.browserExtension.name,
-        children: networkManager.browserExtension?.allAccounts?.map(address => ({ address })) || []
+        children: [{ address: networkManager.browserExtension.currentAccount, name: networkManager.browserExtension.name }]
       }]}
       value={value}
       onChange={onChangeValue}
       disabled={disabled}
+      abbreviationOption
     />
   )
 }
